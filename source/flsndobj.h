3f8c5d988e4a713948b1d6160c8bb427857e54f9
thomas <gr@grrrr.org>
2002-09-19 00:26:53 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/trunk@284 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flsndobj.h
+++ source/flsndobj.h
@@ -1,3 +1,11 @@
+/* 
+
+Copyright (c) 2002 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+
+*/
+
 #ifndef FLEXT_SNDOBJ
 #define FLEXT_SNDOBJ
 

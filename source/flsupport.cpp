e8f9850f51c8e3495db245d2052f8a2642a7ad00
thomas <gr@grrrr.org>
2002-10-17 02:47:12 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/trunk@317 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ source/flsupport.cpp
@@ -0,0 +1,14 @@
+#include "flsupport.h"
+
+const t_symbol *flext::sym_float = NULL;
+const t_symbol *flext::sym_symbol = NULL;
+const t_symbol *flext::sym_bang = NULL;
+const t_symbol *flext::sym_list = NULL;
+const t_symbol *flext::sym_anything = NULL;
+const t_symbol *flext::sym_pointer = NULL;
+const t_symbol *flext::sym_int = NULL;
+
+#ifdef PD
+const t_symbol *flext::sym_signal = NULL;
+#endif
+

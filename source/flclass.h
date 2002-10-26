9299dfe036c32a790858d8ef600fe93991d4c77d
thomas <gr@grrrr.org>
2002-10-26 22:53:59 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/trunk@344 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flclass.h
+++ source/flclass.h
@@ -430,6 +430,7 @@ protected:
 	void AddAttrib(const char *attr,bool (*get)(flext_base *,bool &),bool (*set)(flext_base *,bool &)) { AddAttrib(attr,a_int,(methfun)get,(methfun)set); }
 	void AddAttrib(const char *attr,bool (*get)(flext_base *,t_symbol *&),bool (*set)(flext_base *,t_symbol *&)) { AddAttrib(attr,a_symbol,(methfun)get,(methfun)set); }
 	void AddAttrib(const char *attr,bool (*get)(flext_base *,AtomList *&),bool (*set)(flext_base *,AtomList *&)) { AddAttrib(attr,a_LIST,(methfun)get,(methfun)set); }
+	void AddAttrib(const char *attr,bool (*get)(flext_base *,AtomAnything *&),bool (*set)(flext_base *,AtomAnything *&)) { AddAttrib(attr,a_ANY,(methfun)get,(methfun)set); }
 
 //!		@} 
 

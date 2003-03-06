5a2e00d4b68535d29cccb0c9072645d289809ad3
thomas <gr@grrrr.org>
2003-03-06 20:21:12 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/trunk@549 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flutil.cpp
+++ source/flutil.cpp
@@ -15,32 +15,40 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #include "flext.h"
 #include <string.h>
 
+#if FLEXT_OS == FLEXT_OS_WIN
+#include <windows.h>
+#endif
+
 void flext::CopyMem(void *dst,const void *src,int bytes) 
 {
+#if FLEXT_OS == FLEXT_OS_WIN
+    MoveMemory(dst,src,bytes);
+#else
 	memcpy(dst,src,bytes);
+#endif
 }
 
 void flext::ZeroMem(void *dst,int bytes) 
 {
+#if FLEXT_OS == FLEXT_OS_WIN
+    ZeroMemory(dst,bytes);
+#else
 	memset(dst,0,bytes);
+#endif
 }
 
 
 bool flext::Forward(const t_symbol *recv,const t_symbol *s,int argc,const t_atom *argv)
 {
-	t_class **cl = (t_class **)recv->s_thing;
+	void *cl = recv->s_thing;
 	if(!cl) return false;
     
 #if FLEXT_SYS == FLEXT_SYS_PD
-	pd_forwardmess(cl,argc,(t_atom *)argv);
-    return true;
+	pd_typedmess((t_class **)cl,(t_symbol *)s,argc,(t_atom *)argv);
+#elif FLEXT_SYS == FLEXT_SYS_MAX
+	typedmess(recv->s_thing,(t_symbol *)s,argc,(t_atom *)argv);
 #else
-	if(recv->s_thing) {
-		typedmess(recv->s_thing,(t_symbol *)s,argc,(t_atom *)argv);
-		return true;
-	}
-	else
-		return false;
+#error Not implemented
 #endif
-
+	return true;
 }

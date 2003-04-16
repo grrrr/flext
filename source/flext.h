4ff8dc500cc6bfd89150e7fbc9cafef162cc473c
thomas <gr@grrrr.org>
2003-04-16 10:52:05 +0000

    This commit was manufactured by cvs2svn to create tag 'v0-4-3'.
    git-svn-id: https://svn.grrrr.org/ext/tags/v0-4-3@594 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flext.h
+++ source/flext.h
@@ -26,7 +26,7 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #define FLEXT_VERSION 403
 
 //! \brief flext version string
-#define FLEXT_VERSTR "0.4.3pre"
+#define FLEXT_VERSTR "0.4.3"
 
 //! @}
 

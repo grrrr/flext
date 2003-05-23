081b7f896d801836ea93f68a65e487d1c67c650c
thomas <gr@grrrr.org>
2003-05-23 16:46:33 +0000

    *** empty log message ***
    git-svn-id: https://svn.grrrr.org/ext/trunk@620 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flext.h
+++ source/flext.h
@@ -26,7 +26,7 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #define FLEXT_VERSION 404
 
 //! \brief flext version string
-#define FLEXT_VERSTR "0.4.4pre"
+#define FLEXT_VERSTR "0.4.4"
 
 //! @}
 

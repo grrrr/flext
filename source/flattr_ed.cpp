e82bf790891ca834a989566df3b43e0233ac2efc
thomas <gr@grrrr.org>
2004-09-08 12:34:12 +0000

    modified attribute editor
    git-svn-id: https://svn.grrrr.org/ext/trunk@835 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flattr_ed.cpp
+++ source/flattr_ed.cpp
@@ -78,62 +78,78 @@ void flext_base::SetAttrEditor(t_classid c)
     // generate the script for the property dialog
 
     sys_gui(
-        "proc flext_apply {id alen} {\n"
+        "proc flext_makevalue {id ix} {\n"
             // strip "." from the TK id to make a variable name suffix
             "set vid [string trimleft $id .]\n"
 
+            "set var_attr_name [concat [concat var_name_$ix]_$vid ]\n"
+            "set var_attr_init [concat [concat var_init_$ix]_$vid ]\n"
+            "set var_attr_val [concat [concat var_val_$ix]_$vid ]\n"
+            "set var_attr_save [concat [concat var_save_$ix]_$vid ]\n"
+            "set var_attr_type [concat [concat var_type_$ix]_$vid ]\n"
+
+            "global $var_attr_name $var_attr_init $var_attr_val $var_attr_save $var_attr_type\n"
+
+            "if { [expr $$var_attr_type] != 0 } {\n"
+                // attribute is puttable
+
+                "lappend lst [eval concat $$var_attr_name]\n" 
+
+                // process current value
+                "set tmp [eval concat $$var_attr_val]\n"
+                "set len [llength $tmp]\n"
+                "if { $len == 1 } {\n"
+                    // it's an atom
+                    // if atom starts with $, replace it by # ($ can't be passed by TCL)
+                    "if { [string index $tmp 0] == \"$\" } {\n"
+                        "set tmp [string replace $tmp 0 0 #]\n" 
+                    "}\n"
+                    "lappend lst $tmp\n" 
+                "} else {\n"
+                    // it's a list
+                    "set lst [concat $lst {list} $len $tmp]\n" 
+                "}\n"
+
+                // process init value
+                "set tmp [eval concat $$var_attr_init]\n"
+                "set len [llength $tmp]\n"
+                "if { $len == 1 } {\n"
+                    // it's an atom
+                    // if atom starts with $, replace it by # ($ can't be passed by TCL)
+                    "if { [string index $tmp 0] == \"$\" } {\n"
+                        "set tmp [string replace $tmp 0 0 #]\n" 
+                    "}\n"
+                    "lappend lst $tmp\n" 
+                "} else {\n"
+                    // it's a list
+                    "set lst [concat $lst {list} $len $tmp]\n" 
+                "}\n"
+
+                // return value
+                "return [list [eval concat $$var_attr_save]]\n" 
+            "} else {\n"
+                // return empty list
+                "return [list]\n"
+            "}\n"
+        "}\n"
+
+        "proc flext_apply {id ix} {\n"
+            "set lst [flext_makevalue $id $ix]\n"
+            "set cmd [concat $id attributedialog $lst \\;]\n"
+            "puts $cmd\n"
+            "pd $cmd\n"
+        "}\n"
+
+        "proc flext_applyall {id alen} {\n"
             // make a list of the attribute values (including save flags)
 
             "set lst {}\n"
             "for {set ix 1} {$ix <= $alen} {incr ix} {\n"
-                "set var_attr_name [concat [concat var_name_$ix]_$vid ]\n"
-                "set var_attr_init [concat [concat var_init_$ix]_$vid ]\n"
-                "set var_attr_val [concat [concat var_val_$ix]_$vid ]\n"
-                "set var_attr_save [concat [concat var_save_$ix]_$vid ]\n"
-                "set var_attr_type [concat [concat var_type_$ix]_$vid ]\n"
-
-                "global $var_attr_name $var_attr_init $var_attr_val $var_attr_save $var_attr_type\n"
-        
-                "if { [expr $$var_attr_type] != 0 } {\n"
-                    // attribute is puttable
-
-                    "lappend lst [eval concat $$var_attr_name]\n" 
-
-                    // process current value
-                    "set tmp [eval concat $$var_attr_val]\n"
-                    "set len [llength $tmp]\n"
-                    "if { $len == 1 } {\n"
-                        // it's an atom
-                        // if atom starts with $, replace it by # ($ can't be passed by TCL)
-                        "if { [string index $tmp 0] == \"$\" } {\n"
-                            "set tmp [string replace $tmp 0 0 #]\n" 
-                        "}\n"
-                        "lappend lst $tmp\n" 
-                    "} else {\n"
-                        // it's a list
-                        "set lst [concat $lst {list} $len $tmp]\n" 
-                    "}\n"
-
-                    // process init value
-                    "set tmp [eval concat $$var_attr_init]\n"
-                    "set len [llength $tmp]\n"
-                    "if { $len == 1 } {\n"
-                        // it's an atom
-                        // if atom starts with $, replace it by # ($ can't be passed by TCL)
-                        "if { [string index $tmp 0] == \"$\" } {\n"
-                            "set tmp [string replace $tmp 0 0 #]\n" 
-                        "}\n"
-                        "lappend lst $tmp\n" 
-                    "} else {\n"
-                        // it's a list
-                        "set lst [concat $lst {list} $len $tmp]\n" 
-                    "}\n"
-
-                    "lappend lst [eval concat $$var_attr_save]\n" 
-                "}\n"
+                "lappend lst [flext_makevalue $id $ix]\n" 
             "}\n"
 
             "set cmd [concat $id attributedialog $lst \\;]\n"
+            "puts $cmd\n"
             "pd $cmd\n"
         "}\n"
 
@@ -143,7 +159,7 @@ void flext_base::SetAttrEditor(t_classid c)
         "}\n"
 
         "proc flext_ok {id alen} {\n"
-            "flext_apply $id $alen\n"
+            "flext_applyall $id $alen\n"
             "flext_cancel $id\n"
         "}\n"
 
@@ -229,7 +245,8 @@ void flext_base::SetAttrEditor(t_classid c)
                 "grid columnconfigure $id 0 -weight 1\n"  // label
                 "grid columnconfigure $id {1 4} -weight 3\n" // value entry
                 "grid columnconfigure $id {2 3} -weight 0\n"  // copy buttons
-                "grid columnconfigure $id {5 6 7} -weight 0\n" // radio buttons
+                "grid columnconfigure $id 5 -weight 1\n"  // apply button
+                "grid columnconfigure $id {6 7 8} -weight 0\n" // radio buttons
 
 //                "grid rowconfigure $id {0 1 2} -weight 0\n"
 
@@ -238,6 +255,7 @@ void flext_base::SetAttrEditor(t_classid c)
                 "label $id.init  -text {initial value} -height 2 -font {Helvetica 9 bold}\n"
                 "label $id.copy  -text {copy} -height 2 -font {Helvetica 9 bold}\n"
                 "label $id.val   -text {current value} -height 2 -font {Helvetica 9 bold}\n"
+                "label $id.apply -text {} -height 2 -font {Helvetica 9 bold}\n" // why must this be empty?
                 "foreach {i txt} {0 {don't\rsave} 1 {do\rinit} 2 {always\rsave} } {\n"
                     "label $id.b$i -text $txt -height 2 -font {Helvetica 9 bold}\n"
                 "}\n"
@@ -247,13 +265,14 @@ void flext_base::SetAttrEditor(t_classid c)
                 "grid config $id.init  -column 1 -row $row \n"
                 "grid config $id.copy  -column 2 -columnspan 2 -row $row \n"
                 "grid config $id.val   -column 4 -row $row \n"
-                "foreach i {0 1 2} { grid config $id.b$i -column [expr $i + 5] -row $row }\n"
+                "grid config $id.apply  -column 5 -row $row \n"
+                "foreach i {0 1 2} { grid config $id.b$i -column [expr $i + 6] -row $row }\n"
 //              "grid config $id.options -column 3 -row 0 \n"
                 "incr row\n"
 
                 // Separator
                 "frame $id.sep -relief ridge -bd 1 -height 2\n"
-                "grid config $id.sep -column 0 -columnspan 8 -row $row -pady 2 -sticky {snew}\n"
+                "grid config $id.sep -column 0 -columnspan 9 -row $row -pady 2 -sticky {snew}\n"
                 "incr row\n"
 
                 "set ix 1\n"
@@ -322,10 +341,15 @@ void flext_base::SetAttrEditor(t_classid c)
                         "grid config $id.init-$ix  -column 1 -row $row -padx 5 -sticky {ew}\n"
                         "grid config $id.val-$ix   -column 4 -row $row -padx 5 -sticky {ew}\n"
 
+                        // copy buttons
                         "button $id.b2i-$ix -text {<-} -height 1 -command \" flext_copyval $var_attr_init $var_attr_val \"\n"
                         "grid config $id.b2i-$ix  -column 2 -row $row  -sticky {ew}\n"
                         "button $id.b2c-$ix -text {->} -height 1 -command \" flext_copyval $var_attr_val $var_attr_init \"\n"
                         "grid config $id.b2c-$ix  -column 3 -row $row  -sticky {ew}\n"
+
+                        // apply button
+                        "button $id.apply-$ix -text {Apply} -height 1 -command \" flext_apply $id $ix \"\n"
+                        "grid config $id.apply-$ix  -column 5 -row $row  -sticky {ew}\n"
 
     //                  "tk_optionMenu $id.opt-$ix $var_attr_save {don't save} {initialize} {always save}\n"
     //                  "grid config $id.opt-$ix -column 5 -row $ix \n"
@@ -333,7 +357,7 @@ void flext_base::SetAttrEditor(t_classid c)
                         // radiobuttons
                         "foreach {i c} {0 black 1 blue 2 red} {\n"
                             "radiobutton $id.b$i-$ix -value $i -foreground $c -variable $var_attr_save \n"
-                            "grid config $id.b$i-$ix -column [expr $i + 5] -row $row  \n"
+                            "grid config $id.b$i-$ix -column [expr $i + 6] -row $row  \n"
                         "}\n"
                     "} else {\n"
                         // attribute is gettable only
@@ -361,7 +385,7 @@ void flext_base::SetAttrEditor(t_classid c)
                         "grid config $id.val-$ix -column 4 -row $row -padx 5 -sticky {ew}\n"
 
                         "label $id.readonly-$ix -text \"read-only\"\n"
-                        "grid config $id.readonly-$ix -column 5 -columnspan 3 -row $row -padx 5 -sticky {ew}\n"
+                        "grid config $id.readonly-$ix -column 6 -columnspan 3 -row $row -padx 5 -sticky {ew}\n"
                     "}\n"
 
                     // increase counter
@@ -372,16 +396,16 @@ void flext_base::SetAttrEditor(t_classid c)
                 // Separator
                 "frame $id.sep2 -relief ridge -bd 1 -height 2\n"
 //                "grid rowconfigure $id $row -weight 0\n"
-                "grid config $id.sep2 -column 0 -columnspan 8 -row $row -pady 5 -sticky {snew}\n"
+                "grid config $id.sep2 -column 0 -columnspan 9 -row $row -pady 5 -sticky {snew}\n"
                 "incr row\n"
 
                 // Buttons
                 "frame $id.buttonframe\n"
                 "pack $id.buttonframe -side bottom -fill x\n"
 
-                "button $id.buttonframe.cancel -text {Cancel} -width 20 -command \" flext_cancel $id \"\n"
-                "button $id.buttonframe.apply -text {Apply} -width 20 -command \" flext_apply $id $alen \"\n"
-                "button $id.buttonframe.ok -text {OK} -width 20 -command \" flext_ok $id $alen \"\n"
+                "button $id.buttonframe.cancel -text {Leave} -width 20 -command \" flext_cancel $id \"\n"
+                "button $id.buttonframe.apply -text {Apply all} -width 20 -command \" flext_applyall $id $alen \"\n"
+                "button $id.buttonframe.ok -text {Apply & Leave} -width 20 -command \" flext_ok $id $alen \"\n"
                 "button $id.buttonframe.help -text {Help} -width 10 -command \" flext_help $id \"\n"
 
                 "pack $id.buttonframe.cancel -side left -expand 1\n"
@@ -390,12 +414,12 @@ void flext_base::SetAttrEditor(t_classid c)
                 "pack $id.buttonframe.help -side left -expand 1\n"
 
 //                "grid rowconfigure $id $row -weight 0\n"
-                "grid config $id.buttonframe -column 0 -columnspan 8 -row $row -pady 5 -sticky {ew}\n"
+                "grid config $id.buttonframe -column 0 -columnspan 9 -row $row -pady 5 -sticky {ew}\n"
 
                 // Key bindings
                 "bind $id {<KeyPress-Escape>} \" flext_cancel $id \"\n"
                 "bind $id {<KeyPress-Return>} \" flext_ok $id $alen \"\n"
-                "bind $id {<Shift-KeyPress-Return>} \" flext_apply $id $alen \"\n"
+                "bind $id {<Shift-KeyPress-Return>} \" flext_applyall $id $alen \"\n"
         "}\n"
     );
 }
@@ -622,7 +646,7 @@ void flext_base::cb_GfxSave(t_gobj *c, t_binbuf *b)
 bool flext_base::cb_AttrDialog(flext_base *th,int argc,const t_atom *argv)
 {
     int i = 0;
-    if(IsSymbol(argv[i]) && GetSymbol(argv[i]) == sym_list) ++i;
+    if(GetASymbol(argv[i]) == sym_list) ++i;
 
     for(; i < argc; ) {
         FLEXT_ASSERT(IsSymbol(argv[i]));

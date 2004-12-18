dfd4af381a1f6fe8052ca70f2af99a003b96d959
thomas <gr@grrrr.org>
2004-12-18 12:12:11 +0000

    simplified make system
    git-svn-id: https://svn.grrrr.org/ext/trunk@902 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ buildext.bat
@@ -0,0 +1,41 @@
+@set flext=%~dp0
+
+@rem Arguments:
+@rem %1 - platform (win/lnx/mac)
+@rem %2 - system (pd/max)
+@rem %3 - compiler (msvc/gcc/mingw/cygwin/bcc/icc)
+@rem %4 - target (all/all-debug/all-shared/all-shared-debug/clean/install)
+
+@set platform=%1
+@set rtsys=%2
+@set compiler=%3
+@set target=%4
+
+@ret --- The subbatch knowns which make utility to use ---
+@set subbatch=%flext%build\build-%compiler%.bat
+
+@if "%platform%"=="" goto syntax
+@if "%rtsys%"=="" goto syntax
+@if "%compiler%"=="" goto syntax
+
+@if not exist %subbatch% goto syntax
+
+@call %subpatch% %subbatch% %platform% %rtsys% %target% %5 %6 %7 %8 %9
+
+@goto end
+
+@rem -----------------------------------------
+:syntax
+
+@echo .
+@echo SYNTAX: buildext [platform] [system] [compiler] {target}
+@echo platform ... win / lnx / mac
+@echo system   ... pd / max
+@echo compiler ... msvc / gcc / mingw / cygwin / bcc / icc
+@echo target   ... all (default) / all-debug / all-shared / all-shared-debug / 
+@echo              clean / install
+@echo .
+@echo Please make sure that your make program and compiler can be accessed with the
+@echo system path and that all relevant environment variables are properly set.
+
+:end

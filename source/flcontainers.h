2b8ffae785e910f1fdab96ca69eab4917695cfd1
thomas <gr@grrrr.org>
2005-03-07 08:45:10 +0000

    updated flext lock-free containers
    git-svn-id: https://svn.grrrr.org/ext/trunk@1019 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flcontainers.h
+++ source/flcontainers.h
@@ -276,6 +276,7 @@ class PooledLifo
 {
 public:
     inline T *New() { T *n = reuse.Pop(); return n?n:new T; }
+    inline size_t Size() const { return TypedLifo<T>::Size(); }
     inline void Free(T *p) { if(reuse.Size() < Size()*M+O) reuse.Push(p); else delete p; }
 private:
     TypedLifo<T> reuse;
@@ -353,6 +354,7 @@ class PooledFifo
 {
 public:
     inline T *New() { T *n = reuse.Pop(); return n?n:new T; }
+    inline size_t Size() const { return TypedLifo<T>::Size(); }
     inline void Free(T *p) { if(reuse.Size() < Size()*M+O) reuse.Push(p); else delete p; }
 private:
     TypedLifo<T> reuse;

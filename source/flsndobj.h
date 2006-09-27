b97010a81dd13db076a08c5bba1316809cfaca02
thomas <gr@grrrr.org>
2006-09-27 07:51:35 +0000

    fixed SndObjs include files
    git-svn-id: https://svn.grrrr.org/ext/trunk@1175 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flsndobj.h
+++ source/flsndobj.h
@@ -2,7 +2,7 @@
 
 flext - C++ layer for Max/MSP and pd (pure data) externals
 
-Copyright (c) 2001-2005 Thomas Grill (gr@grrrr.org)
+Copyright (c) 2001-2006 Thomas Grill (gr@grrrr.org)
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "license.txt," in this distribution.  
 
@@ -16,7 +16,8 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #ifndef FLEXT_THREADS
 #define NOPTHREAD
 #endif
-#include <SndObj/AudioDefs.h>
+#include <SndObj/SndObj.h>
+#include <SndObj/SndIO.h>
 #undef NOPTHREAD
 
 

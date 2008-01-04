6962bf47c44a2e1bdaa1a275f40704a2c2664e86
thomas <gr@grrrr.org>
2008-01-04 13:57:14 +0000

    64-bit compilation fixes
    git-svn-id: https://svn.grrrr.org/ext/trunk@1217 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/flutil.cpp
+++ source/flutil.cpp
@@ -2,7 +2,7 @@
 
 flext - C++ layer for Max/MSP and pd (pure data) externals
 
-Copyright (c) 2001-2005 Thomas Grill (gr@grrrr.org)
+Copyright (c) 2001-2008 Thomas Grill (gr@grrrr.org)
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "license.txt," in this distribution.  
 
@@ -29,10 +29,10 @@ void flext::CopyMem(void *dst,const void *src,int bytes)
 {
 #if FLEXT_OS == FLEXT_OS_WIN
     MoveMemory(dst,src,bytes);
-#elif FLEXT_OS == FLEXT_OS_MAC
-    BlockMoveData(src,dst,bytes);
+#elif FLEXT_OS == FLEXT_OS_MAC && !defined(__LP64__)
+    BlockMoveData(src,dst,bytes);   // not available for 64 bits
 #else
-    memcpy(dst,src,bytes);
+    memmove(dst,src,bytes);
 #endif
 }
 
@@ -41,7 +41,11 @@ void flext::ZeroMem(void *dst,int bytes)
 #if FLEXT_OS == FLEXT_OS_WIN
     ZeroMemory(dst,bytes);
 #elif FLEXT_OS == FLEXT_OS_MAC
+#   ifdef __LP64__  // 64 bits compilation
+    bzero(dst,bytes);
+#   else
     BlockZero(dst,bytes);
+#   endif
 #else
     memset(dst,0,bytes);
 #endif

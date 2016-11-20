d2d2caaff60b8454e5fee33a15265e594491972b
Thomas Grill <gr@grrrr.org>
2016-11-20 01:15:03 +0100

    C74_X64 definition is set in Max SDK

--- source/flprefix.h
+++ source/flprefix.h
@@ -351,7 +351,6 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 //  #pragma message("Compiling for Max/MSP")
 #ifndef MSP64
 #if FLEXT_CPU == FLEXT_CPU_X86_64
-#define C74_X64 1
 #define MSP64 1
 #else
 #define MSP64 0
